14ab76f232f31bf7f554b9a1104299ab802e9f26
GauravMakasare <gauravmakasare115@gmail.com>
2022-03-01 12:31:52 +0530

    Update northwind.sql

--- TRAINING2022/Week 1 SQL/northwind.sql
+++ TRAINING2022/Week 1 SQL/northwind.sql
@@ -11,6 +11,7 @@ GROUP BY
     contactname;
 
 --2. Find suppliers who sell more than one product to Northwind Trader. 
+ 
 
 
 --3. Create a function to get latest order date for entered customer_id 
@@ -170,16 +171,123 @@ ORDER BY
 
 --13. Sales by Category: For each category, we get the list of products sold and the total sales amount.
 
+SELECT 
+    c.categoryid, 
+    c.categoryname, 
+    p.productname, 
+    SUM(od.unitprice * od.quantity * (1 - od.discount)) AS productsales
+FROM 
+    categories c, 
+    products p, 
+    orderdetails od 
+WHERE 
+    c.categoryid=p.categoryid 
+    AND 
+    od.productid=p.productid 
+GROUP BY 
+    c.categoryid, 
+    c.categoryname, 
+    p.productname
+ORDER BY 
+    c.categoryid, 
+    p.productname;
+
 --14. Create below views:
 --        1.  vwProducts_Above_Average_Price
 --            Displays products(productname,unitprice) who’s price is greater than avg(price)
+CREATE VIEW vwProducts_Above_Average_Price
+AS
+    SELECT 
+        productname, 
+        unitprice
+    FROM 
+        products
+    WHERE 
+        unitprice>(SELECT 
+                   AVG(unitprice) 
+               FROM 
+                   products)
+ORDER BY 
+    unitprice;
+
+SELECT * FROM vwProducts_Above_Average_Price;
+
 --        2.  vwQuarterly_Orders_by_Product
 --            Display product(productname), customers(companyname), orders(orderyear) 
+
+CREATE VIEW vwQuarterly_Orders_by_Product
+AS
+SELECT 
+    p.productname, 
+    c.companyname, 
+    EXTRACT (Year FROM o.orderdate) AS orderyear
+FROM 
+    products p, 
+    customers c, 
+    orders o, 
+    orderdetails od
+WHERE 
+    c.customerid=o.customerid AND o.orderid=od.orderid AND od.productid=p.productid
+ORDER BY 
+    c.companyname, 
+    p.productname;
+
+SELECT * FROM vwQuarterly_Orders_by_Product;
+
 --        3. vwUnitsInStock
 --            Display Supplier Continent wise sum of unitinstock.
 --            'Europe'= ('UK','Spain','Sweden','Germany','Norway', 'Denmark','Netherlands','Finland','Italy','France')
 --            'America'= 'USA','Canada','Brazil' and 'Asia-Pacific'
+
+CREATE VIEW vwUnitsInStock
+AS
+SELECT 
+    sup.supplier_continent, 
+    SUM(p.unitsinstock) AS continent_sum
+FROM 
+    products p, 
+    (SELECT s.supplierid, 
+                    CASE
+                        WHEN s.country IN ('UK','Spain','Sweden','Germany','Norway', 'Denmark','Netherlands','Finland','Italy','France')
+                            THEN 'Europe'
+                        WHEN s.country IN ('USA','Canada','Brazil', 'Asia-Pacific')
+                            THEN 'America'
+                        WHEN s.country NOT IN ('UK','Spain','Sweden','Germany','Norway', 'Denmark','Netherlands','Finland','Italy','France', 'USA','Canada','Brazil', 'Asia-Pacific')
+                            THEN 'Others'
+                        END AS supplier_continent 
+                  FROM suppliers s) sup
+WHERE 
+    sup.supplierid=p.supplierid
+GROUP BY 
+    sup.supplier_continent;
+
+SELECT * FROM vwUnitsInStock;
+
+
 --        4. vw10Most_Expensive_Products
 --            Display top 10 expensive products 
+
+CREATE VIEW vw10Most_Expensive_Products
+AS
+SELECT 
+    productname, unitprice
+FROM 
+    products
+ORDER BY 
+    unitprice DESC
+FETCH FIRST 10 ROWS ONLY;
+
+SELECT * FROM vw10Most_Expensive_Products;
+
 --        5. vwCustomer_Supplier_by_City
 --            Display customer supplier by city
+CREATE VIEW vwCustomer_Supplier_by_City
+AS
+(SELECT companyname, city, contactname, 'Customers' AS relationship
+FROM customers)
+UNION 
+(SELECT companyname, city, contactname, 'Suppliers' AS relationship
+FROM suppliers) 
+ORDER BY city;
+
+SELECT * FROM vwCustomer_Supplier_by_City;
